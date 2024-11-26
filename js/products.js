1afbb05e0b6803fdfb918e0e5f188a8498565708
Hwang Tae Young <hty0525@naver.com>
2024-11-27 01:11:22 +0900

    api 이미지 비율 이슈로 height 원복

--- js/products.js
+++ js/products.js
@@ -21,7 +21,6 @@ function displayProducts(products) {
 		img.src = product.image;
 		img.alt = `product: ${product.title}`;
 		img.width = 250;
-		img.height = 250;
 		pictureDiv.appendChild(img);
 
 		// Create the product info div
